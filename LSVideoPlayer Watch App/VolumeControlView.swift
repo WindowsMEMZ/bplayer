fbd5033ae7a18ead44fb53c929fcb06be7cc7ab4
Lightning-Lion <zj56789012@outlook.com>
2024-01-23 13:29:16 +0800

    修复了音量调节功能

--- LSVideoPlayer Watch App/VolumeControlView.swift
+++ LSVideoPlayer Watch App/VolumeControlView.swift
@@ -6,10 +6,19 @@
 //
 
 import SwiftUI
-
-struct  音量调节视图: View {
-    var body: some View {
-        Image(systemName: "speaker.wave.3.fill")
+import WatchKit
+struct 音量调节视图: WKInterfaceObjectRepresentable {
+    typealias WKInterfaceObjectType = WKInterfaceVolumeControl
+    
+    
+    func makeWKInterfaceObject(context: WKInterfaceObjectRepresentableContext<音量调节视图>) -> WKInterfaceVolumeControl {
+        // Return the interface object that the view displays.
+        return WKInterfaceVolumeControl(origin: .local)
+    }
+    
+    func updateWKInterfaceObject(_ map: WKInterfaceVolumeControl, context: WKInterfaceObjectRepresentableContext<音量调节视图>) {
+        
     }
 }
+
 
